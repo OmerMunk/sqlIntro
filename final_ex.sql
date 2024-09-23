11bb3a769fc9b2074e6baedc83c8218bc05b3327
Omer Munk <93129669+OmerMunk@users.noreply.github.com>
2024-09-23 15:16:52 +0300

    Create final_ex.sql

---
+++ final_ex.sql
@@ -0,0 +1,44 @@
+-- Exercise 1: Find clients who rented more than 3 different car types in total.
+
+
+-- Exercise 2: List all cars and how many times each car was rented, even if a car has not been rented yet.
+
+
+-- Exercise 3: Find clients who have paid more than 5000 in total for all their rentals after 2023.
+
+
+-- Exercise 4: List all cars rented by 'David Green' but never rented by 'Sarah Blue'.
+
+
+-- Exercise 5: Find the client who rented cars for the longest combined period in total.
+
+
+-- Exercise 6: List clients who rented a car for more than 30 days but didn't pay for it yet.
+
+
+-- Exercise 7: Find cars that have never been rented.
+
+
+-- Exercise 8: Find the car type that generated the highest total payments.
+
+
+-- Exercise 9: List all clients who rented a car in February 2024 and paid more than 2000 in total.
+
+
+-- Exercise 10: Display clients who rented at least 3 different cars and paid for all of them.
+
+
+-- Exercise 11: Find clients who rented cars in both January and March of 2024.
+
+
+-- Exercise 12: List clients whose total payment is below 2000, except those who rented a BMW.
+
+
+-- Exercise 13: Find the car rented for the most days in 2024 and the total rental period.
+
+
+-- Exercise 14: List clients who have an open rental and have paid for more than one order.
+
+
+-- Exercise 15: Find the total payment for clients who rented a car for more than 10 days and ordered an Audi.
+
